d749d9c21c9ff996de9b70031b88dd7ad1efcf61
mustafa <mustafanaiby@gmail.com>
2023-02-08 07:28:06 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="logo">
+            <h2>Mustafa</h2>
+          </div>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Service</a>
+            </li>
+          </ul>
+        </div>
+      </header>
+
+      <main>
+        <div className="contain">
+          <div className="Card">
+            <img src="" alt="ddd" />
+            <h2>Mustafa</h2>
+          </div>
+          <div className="Card">
+            <img src="" alt="ddd" />
+            <h2>Mustafa</h2>
+          </div>
+          <div className="Card">
+            <img src="" alt="ddd" />
+            <h2>Mustafa</h2>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+export default App;
