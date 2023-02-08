b569567c6910eabaf59f1f754293768a917ad562
mustafa <mustafanaiby@gmail.com>
2023-02-08 10:39:07 +0330

    d

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,17 @@ import React from "react";
 function App() {
   return (
     <>
-      <header>
-        <div className="container">
-          <div className="logo">
-            <h2>Mustafa</h2>
-          </div>
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">Service</a>
-            </li>
-          </ul>
+      <div className="e">
+        <div className="conain">
+          <h2>This is Header Box</h2>
         </div>
-      </header>
-
-      <main>
-        <div className="contain">
-          <div className="Card">
-            <img src="" alt="ddd" />
-            <h2>Mustafa</h2>
-          </div>
-          <div className="Card">
-            <img src="" alt="ddd" />
-            <h2>Mustafa</h2>
-          </div>
-          <div className="Card">
-            <img src="" alt="ddd" />
-            <h2>Mustafa</h2>
-          </div>
+        <div className="conain">
+          <h2>This is Main box</h2>
         </div>
-      </main>
+        <div className="conain">
+          <h2>This is footer box</h2>
+        </div>
+      </div>
     </>
   );
 }
