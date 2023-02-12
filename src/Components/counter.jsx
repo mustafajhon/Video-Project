ee0fba67b0eb0daa8a75e55d04209d57cee2dee5
mustafa <mustafanaiby@gmail.com>
2023-02-12 07:46:34 +0330

    Completed

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -2,8 +2,38 @@ import React, { useState } from "react";
 
 function Counter() {
   const [count, func] = useState(0);
+
+  function Welcom() {
+    let head = document.getElementById("head");
+    head.textContent = "Goals is for Men, Dreams is for Kids.";
+  }
+
   return (
     <>
+      <h1 id="head"></h1>
+
+      <div className="container1">
+        <div className="box1 left">
+          <div className="red">
+            <div className="span">
+              <span>Goals</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="btn">
+          <button onClick={Welcom}>Click Here</button>
+        </div>
+
+        <div className="box1 right">
+          <div className="red">
+            <div className="span">
+              <span>Dreams</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="container">
         <button
           onClick={() => func(count - 1)}
